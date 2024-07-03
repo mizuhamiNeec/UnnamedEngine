fa947cf647d5c2eb80ee3e28ef90736630670896
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2024-07-03 15:02:51 +0900

    03_02_確認課題

--- Source/Engine/Window/Window.h
+++ Source/Engine/Window/Window.h
@@ -44,7 +44,9 @@ public:
 
 	HWND GetHWND() const { return hWnd_; }
 
-	HINSTANCE GetHInstance() const { return wc_.hInstance; }
+	HINSTANCE GetHInstance() const {
+		return wc_.hInstance;
+	}
 
 private:
 	HWND hWnd_ = nullptr;
