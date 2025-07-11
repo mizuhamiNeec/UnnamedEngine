37edd9d9fb7916f2f3e9b529e228de72667d5d40
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-07-11 14:29:43 +0900

    Add skeletal mesh loading and rendering to EmptyScene

--- Source/Game/Scene/EmptyScene.h
+++ Source/Game/Scene/EmptyScene.h
@@ -6,13 +6,15 @@
 
 class EmptyScene : public BaseScene {
 public:
-    ~EmptyScene() override;
-    void Init() override;
-    void Update(float deltaTime) override;
-    void Render() override;
-    void Shutdown() override;
+	~EmptyScene() override;
+	void Init() override;
+	void Update(float deltaTime) override;
+	void Render() override;
+	void Shutdown() override;
 
 private:
-    D3D12* renderer_ = nullptr;
-    std::unique_ptr<CubeMap> cubeMap_;
+	D3D12*                   renderer_ = nullptr;
+	std::unique_ptr<CubeMap> cubeMap_;
+
+	std::unique_ptr<Entity> skeletalMeshEntity_;
 };
