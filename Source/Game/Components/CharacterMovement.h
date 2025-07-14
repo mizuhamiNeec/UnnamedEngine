cb284db44328be547ed818dd89e33972b643c358
MizuHami <laptop@MizuHami>
2025-07-14 16:46:50 +0900

    キャラクターの共通処理とプレイヤーの処理を分けました

--- Source/Game/Components/CharacterMovement.h
+++ Source/Game/Components/CharacterMovement.h
@@ -30,44 +30,23 @@ public:
 
 protected:
 	TransformComponent* transform_ = nullptr;
-
+	
 	float deltaTime_ = 0.0f;
-
 	Vec3 position_ = Vec3::zero;
 	Vec3 velocity_ = Vec3::zero;
-
+	
 	float mSpeed   = 0.0f;
 	float mJumpVel = 0.0f;
-
+	
 	bool bIsGrounded   = false;
 	bool bWasGrounded  = false;
 	Vec3 mGroundNormal = Vec3::zero;
-
+	
 	const float kDefaultHeightHU = 73.0f;
 	const float kDefaultWidthHU  = 33.0f;
-
 	float mCurrentHeightHU = 73.0f;
 	float mCurrentWidthHU  = 33.0f;
-
+	
 	bool bWishCrouch  = false;
 	bool bIsCrouching = false;
-
-	enum SlideState {
-		NotSliding,
-		Sliding,
-		SlideRecovery
-	};
-
-	SlideState slideState         = NotSliding;
-	float      slideTimer         = 0.0f;
-	float      slideRecoveryTimer = 0.0f;
-
-	const float slideMinSpeed     = 180.0f; // スライドを維持するための最小速度
-	const float slideBoost        = 1.5f;   // スライド中の加速量
-	const float slideFriction     = 0.25f;  // スライド中の摩擦係数
-	const float maxSlideTime      = 1.0f;   // スライドの最大維持時間(秒)
-	const float slideRecoveryTime = 0.5f;   // スライド後の回復時間(秒)
-	const float slideDownForce    = 150.0f; // スライド中の下方向への力
-
-	Vec3 mSlideDir = Vec3::zero; // スライド中の方向ベクトル
 };
