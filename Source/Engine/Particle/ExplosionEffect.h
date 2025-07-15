b6ecb97362666a0c1b28e28334e477f30c51d428
MizuHami <laptop@MizuHami>
2025-07-15 15:22:49 +0900

    ♻️ ExplosionEffectクラスのメンバ変数をmプレフィックス付きにリファクタリング・可読性向上

--- Source/Engine/Particle/ExplosionEffect.h
+++ Source/Engine/Particle/ExplosionEffect.h
@@ -15,8 +15,8 @@ public:
 	void Init(ParticleManager* particleManager, const std::string& texturePath);
 
 	void SetColorGradient(const Vec4& startColor, const Vec4& endColor) {
-		startColor_ = startColor;
-		endColor_   = endColor;
+		mStartColor = startColor;
+		mEndColor   = endColor;
 	}
 
 	void TriggerExplosion(const Vec3& position, const Vec3& normal,
@@ -25,8 +25,8 @@ public:
 	void Draw() const;
 
 private:
-	Vec4 startColor_;
-	Vec4 endColor_;
+	Vec4 mStartColor;
+	Vec4 mEndColor;
 
-	std::unique_ptr<ParticleObject> explosionParticleObject;
+	std::unique_ptr<ParticleObject> mExplosionParticleObject;
 };
