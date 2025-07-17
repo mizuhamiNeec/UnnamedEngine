b29ce2c619af1380ede3d7517522d41bb4a2391c
MizuHami <laptop@MizuHami>
2025-07-17 11:16:13 +0900

    ✨ Console: UnnamedConVarBaseの新規追加・IsCommandのオーバーライド実装

---
+++ Source/Engine/private/Core/Console/UnnamedConVarBase.cpp
@@ -0,0 +1,7 @@
+﻿#include <public/Core/Console/UnnamedConVarBase.h>
+
+namespace Core {
+	bool UnnamedConVarBase::IsCommand() const {
+		return false; // 変数はコマンドじゃあないッ!
+	}
+}
---
+++ Source/Engine/public/Core/Console/UnnamedConVarBase.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <public/Core/Console/UnnamedConCommandBase.h>
+
+namespace Core {
+	class UnnamedConVarBase : public UnnamedConCommandBase {
+	public:
+		// 基底クラスのコンストラクタを呼び出す この書き方知らんかった...
+		using UnnamedConCommandBase::UnnamedConCommandBase;
+
+		~UnnamedConVarBase() override = default;
+
+		[[nodiscard]] bool IsCommand() const override;
+	};
+}
