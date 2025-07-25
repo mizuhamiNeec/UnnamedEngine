bc9b2593eff7e0966edc63817ce11912434392b3
MizuHami <laptop@MizuHami>
2025-07-25 14:50:03 +0900

    🔥 不要なソースを削除

--- Source/Engine/Assets/Asset/Base/Asset.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-class Asset {
-public:
-	virtual ~Asset() = default;
-	[[nodiscard]] virtual const std::string& GetID() const = 0;
-};
-
--- Source/Engine/EntitySystem/RuntimeEntitySystem.cpp
+++
@@ -1,9 +0,0 @@
-#include "RuntimeEntitySystem.h"
-RuntimeEntitySystem::~RuntimeEntitySystem() {}
-void RuntimeEntitySystem::Init() {}
-void RuntimeEntitySystem::Update(float deltaTime) {
-	deltaTime;
-}
-void RuntimeEntitySystem::Render(ID3D12GraphicsCommandList* commandList) {
-	commandList;
-}
