85f4d297c599934d5bef1552e2d41a272b4c70a4
MizuHami <laptop@MizuHami>
2025-07-28 16:15:55 +0900

    ✨ フレームレート制限用のFrameLimiterクラスを新規追加

---
+++ src/engine/private/time/FrameLimiter.cpp
@@ -0,0 +1,56 @@
+﻿#include <engine/public/OldConsole/ConVarManager.h>
+#include <engine/public/time/FrameLimiter.h>
+#include <engine/public/time/GameTime.h>
+#include <engine/public/utils/Properties.h>
+
+FrameLimiter::FrameLimiter(GameTime* gameTime) :
+	mGameTime(gameTime) {
+	SetTargetFPS(kDefaultFpsMax);
+}
+
+void FrameLimiter::SetTargetFPS(const double targetFPS) {
+	using namespace std::chrono;
+	if (targetFPS > 0) {
+		// 1/FPS 秒を duration に変換
+		mTargetFrameDuration = duration_cast<Clock::duration>(
+			duration<double>(1.0 / targetFPS));
+	} else {
+		mTargetFrameDuration = Clock::duration::zero();
+	}
+}
+
+void FrameLimiter::Limit() {
+	CheckConVarValue();
+	
+	// 制限が設定されていない場合は何もしない
+	if (mTargetFrameDuration == Clock::duration::zero()) {
+		return;
+	}
+
+	using namespace std::chrono;
+
+	const double frameSecond  = mGameTime->DeltaTime<double>();
+	const double targetSecond = duration<double>(mTargetFrameDuration).count();
+	const double waitSecond   = targetSecond - frameSecond;
+
+	// フレーム時間が目標時間を超えている場合は待機しない
+	if (waitSecond <= 0.0) {
+		return;
+	}
+
+	const auto waitDuration = duration<double>(waitSecond);
+
+	// 大まかにスリープする
+	constexpr auto spinThreshold = milliseconds(2);
+	if (waitDuration > spinThreshold) {
+		std::this_thread::sleep_for(waitDuration - spinThreshold);
+	}
+
+	const auto endTime = Clock::now() + waitDuration;
+	while (Clock::now() < endTime);
+}
+
+void FrameLimiter::CheckConVarValue() {
+	double targetFPS = ConVarManager::GetConVar("fps_max")->GetValueAsDouble();
+	SetTargetFPS(targetFPS);
+}
---
+++ src/engine/public/time/FrameLimiter.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <chrono>
+
+class GameTime;
+
+//-----------------------------------------------------------------------------
+// Purpose: フレームレートの制限をするためのクラス...
+//			と思ったけど不正確...
+// TODO: 修正しよう
+//
+//-----------------------------------------------------------------------------
+class FrameLimiter {
+public:
+	explicit FrameLimiter(GameTime* gameTime);
+
+	void SetTargetFPS(double targetFPS);
+
+	void Limit();
+
+private:
+	void CheckConVarValue();
+
+	using Clock     = std::chrono::steady_clock;
+	using TimePoint = Clock::time_point;
+
+	GameTime* mGameTime = nullptr;
+
+	Clock::duration mTargetFrameDuration;
+};
