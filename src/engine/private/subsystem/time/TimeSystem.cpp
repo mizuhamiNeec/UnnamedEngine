b46e598cf0253804719c3a71c3b33e731c512fd4
MizuHami <laptop@MizuHami>
2025-07-29 17:59:16 +0900

    ✨ TimeSystem初期化時にServiceLocatorへ登録する処理を追加

--- src/engine/private/subsystem/time/TimeSystem.cpp
+++ src/engine/private/subsystem/time/TimeSystem.cpp
@@ -1,9 +1,12 @@
 #include <engine/public/subsystem/time/TimeSystem.h>
+#include <engine/public/subsystem/interface/ServiceLocator.h>
 
 TimeSystem::~TimeSystem() {
 }
 
 bool TimeSystem::Init() {
+	ServiceLocator::Register<TimeSystem>(this);
+
 	mGameTime     = std::make_unique<GameTime>();
 	mFrameLimiter = std::make_unique<FrameLimiter>(mGameTime.get());
 	mSystemClock  = std::make_unique<SystemClock>();
