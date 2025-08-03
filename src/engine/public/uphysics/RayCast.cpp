6f45f226d53c8b7c46c6f9ca17e4f7ff3bada4f8
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-08-03 12:54:57 +0900

    ♻️ RayCastクラスを実装: 三角形とのレイキャスト判定メソッドを追加

---
+++ src/engine/public/uphysics/RayCast.cpp
@@ -0,0 +1,24 @@
+﻿#include <engine/public/uphysics/CollisionDetection.h>
+#include <engine/public/uphysics/RayCast.h>
+
+bool UPhysics::RayCast::TestTriangle(
+	const Triangle& tri,
+	const Vec3&     dir,
+	const float     length,
+	float&          toi,
+	Vec3&           normal
+) const {
+	float t   = length;
+	const Ray   ray = {
+		start,
+		dir,
+		invDir,
+		0.0f,
+		length
+	};
+	if (!TriangleVsRay(tri, ray, t, normal)) {
+		return false; // 交差しなかった
+	}
+	toi = t / length;
+	return true;
+}
---
+++ src/engine/public/uphysics/RayCast.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <engine/public/uphysics/ShapeCast.h>
+
+namespace UPhysics {
+	struct RayCast final : ShapeCast {
+		[[nodiscard]] AABB ExpandNode(const AABB& nodeBounds) const override {
+			return nodeBounds;
+		}
+
+		bool TestTriangle(
+			const Triangle& tri,
+			const Vec3&     dir,
+			float           length,
+			float&          toi,
+			Vec3&           normal
+		) const;
+
+		Vec3 start;
+		Vec3 invDir;
+	};
+}
