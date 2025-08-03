16920cb0b2bb75ab3f163c85295c083cc811967b
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-08-03 12:55:02 +0900

    ♻️ ShapeCastクラスを実装: AABBの拡張と三角形テスト機能を追加

---
+++ src/engine/public/uphysics/ShapeCast.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <engine/public/uphysics/Primitives.h>
+
+namespace UPhysics {
+	struct ShapeCast {
+		virtual                    ~ShapeCast() = default;
+		[[nodiscard]] virtual AABB ExpandNode(const AABB& nodeBounds) const = 0;
+
+		bool TestTriangle(
+			const Triangle& tri,
+			const Vec3&     dir,
+			float           length,
+			float&          toi,
+			Vec3&           normal
+		);
+	};
+}
