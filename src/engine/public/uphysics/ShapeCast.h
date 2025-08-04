d90e353024b661cd66ea7e2de32e99bb07902c5c
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-08-04 14:00:26 +0900

    ♻️ ShapeCastクラスのTestTriangle関数を仮想関数として定義し、const修飾子を追加

--- src/engine/public/uphysics/ShapeCast.h
+++ src/engine/public/uphysics/ShapeCast.h
@@ -6,12 +6,12 @@ namespace UPhysics {
 		virtual                    ~ShapeCast() = default;
 		[[nodiscard]] virtual AABB ExpandNode(const AABB& nodeBounds) const = 0;
 
-		bool TestTriangle(
+		virtual bool TestTriangle(
 			const Triangle& tri,
 			const Vec3&     dir,
 			float           length,
 			float&          toi,
 			Vec3&           normal
-		);
+		) const = 0;
 	};
 }
