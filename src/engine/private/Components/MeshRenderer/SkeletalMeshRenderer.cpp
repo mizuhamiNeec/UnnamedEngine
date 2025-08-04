b1ef9308c04ab5e9092636b82b80325bcb1446f3
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-08-04 14:00:30 +0900

    ♻️ SkeletalMeshRenderer.cppのマテリアルデータの金属度と粗さの値を調整

--- src/engine/private/Components/MeshRenderer/SkeletalMeshRenderer.cpp
+++ src/engine/private/Components/MeshRenderer/SkeletalMeshRenderer.cpp
@@ -67,8 +67,8 @@ void SkeletalMeshRenderer::OnAttach(Entity& owner) {
 
 		mMaterialData            = mMatParamCBV->GetPtr<MatParam>();
 		mMaterialData->baseColor = {0.5f, 0.5f, 0.5f, 1.0f};
-		mMaterialData->metallic  = 0.25f;
-		mMaterialData->roughness = 0.5f;
+		mMaterialData->metallic  = 0.7f;
+		mMaterialData->roughness = 1.0f;
 		mMaterialData->emissive  = {0.0f, 0.0f, 0.0f};
 	}
 
