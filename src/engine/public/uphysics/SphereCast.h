04ffacc9e68b1a06aeeb0bc493e3fb233e828377
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-08-04 14:00:35 +0900

    ♻️ SphereCastクラスを新規作成し、球体キャスト機能を実装

---
+++ src/engine/public/uphysics/SphereCast.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "ShapeCast.h"
+
+namespace UPhysics {
+	struct SphereCast final : ShapeCast {
+		[[nodiscard]] AABB ExpandNode(const AABB& nodeBounds) const override;
+
+		bool TestTriangle(
+			const Triangle& triangle,
+			const Vec3&     dir,
+			float           length,
+			float&          outTOI,
+			Vec3&           outNormal
+		) const override;
+
+		Vec3  center;
+		float radius;
+	};
+}
