84623b330667d6a603bd3a8e2b978d3126ace342
MizuHami <laptop@MizuHami>
2025-08-05 19:38:29 +0900

    ✨️ Hit構造体にdepthフィールドを追加し、tの初期化を整形しました

--- src/engine/public/uphysics/Primitives.h
+++ src/engine/public/uphysics/Primitives.h
@@ -23,7 +23,8 @@ namespace UPhysics {
 
 	// ヒット情報
 	struct Hit {
-		float    t = FLT_MAX;
+		float    t     = FLT_MAX;
+		float    depth = 0.0f;
 		Vec3     pos;
 		Vec3     normal;
 		uint32_t triIndex = UINT_FAST32_MAX;
