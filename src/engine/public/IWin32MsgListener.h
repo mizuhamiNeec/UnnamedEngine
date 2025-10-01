a5ea234f2670e74184d67e4b319c2bd4591ce1d4
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-10-01 19:37:32 +0900

    ✨ IWin32MsgListenerインターフェースを追加: Win32メッセージを受け取るためのリスナーを定義

---
+++ src/engine/public/IWin32MsgListener.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <Windows.h>
+
+namespace Unnamed {
+	/// @brief Win32のメッセージを受け取るリスナーのインターフェース
+	struct IWin32MsgListener {
+		virtual bool OnWin32Message(HWND, UINT, WPARAM, LPARAM) = 0;
+	};
+}
