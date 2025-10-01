3598658a56a9ed90d714399c75f15318b8a72a56
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-10-02 06:51:18 +0900

    ✨ VertexFormatsを追加

---
+++ src/engine/VertexFormats.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include <runtime/core/math/Math.h>
+
+namespace Unnamed {
+	struct VertexPNUV {
+		Vec3 position;
+		Vec3 normal;
+		Vec2 uv;
+	};
+
+	static_assert(sizeof(VertexPNUV) == 32, "Vertex layout mismatch");
+}
