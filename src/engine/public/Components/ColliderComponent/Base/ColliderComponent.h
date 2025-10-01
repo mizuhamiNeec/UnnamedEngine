8c0a4eaf6f4eb44dccfbf60a0752606f61a5b262
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-10-01 19:37:59 +0900

    ✨ コライダーコンポーネントのリファクタリング: 不要なBoxCastおよびRayCastメソッドを削除し、インクルードパスを整理

--- src/engine/public/Components/ColliderComponent/Base/ColliderComponent.h
+++ src/engine/public/Components/ColliderComponent/Base/ColliderComponent.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <engine/public/Components/base/Component.h>
-#include <engine/public/Physics/Physics.h>
-#include <math/public/MathLib.h>
+#include <runtime/core/math/Math.h>
 
 class PhysicsEngine;
 
@@ -12,26 +11,12 @@ public:
 	void Update(float deltaTime) override = 0;
 	void DrawInspectorImGui() override = 0;
 
-	[[nodiscard]] virtual AABB GetBoundingBox() const = 0;
 	virtual bool CheckCollision(const ColliderComponent* other) const = 0;
 	virtual bool IsDynamic() = 0;
 
 	void SetPhysicsEngine(PhysicsEngine* physicsEngine) {
 		mPhysicsEngine = physicsEngine;
 	}
-
-	[[nodiscard]] std::vector<HitResult> BoxCast(
-		const Vec3& start,
-		const Vec3& direction,
-		float       distance,
-		const Vec3& halfSize
-	) const;
-
-	[[nodiscard]] std::vector<HitResult> RayCast(
-		const Vec3& start,
-		const Vec3& direction,
-		float       distance
-	) const;
 
 	PhysicsEngine* GetPhysicsEngine() const;
 
