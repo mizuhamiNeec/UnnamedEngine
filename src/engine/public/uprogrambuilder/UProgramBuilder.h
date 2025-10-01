2ed2eddb08f5920c3b4cf164568ac3056f69c833
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-10-01 20:31:25 +0900

    ✨ UProgramBuilderの実装: HLSLプログラムを生成するためのビルド機能を追加

---
+++ src/engine/public/uprogrambuilder/UProgramBuilder.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace Unnamed {
+	struct ProgramResources {
+		std::vector<std::string>             textures;
+		std::unordered_map<std::string, int> paramSlots;
+	};
+
+	struct GeneratedProgram {
+		std::string      hlsl; // 完成したものがこちらになりまーす!
+		std::string      vsEntry = "VSMain";
+		std::string      psEntry = "PSMain";
+		ProgramResources resources;
+		uint64_t         hash = 0;
+	};
+
+	struct ProgramBuildInput {
+		std::string bodyPath; // 実装
+		std::string metaPath; // JSON
+		std::vector<std::string> defines;
+		std::string abiInclude = "./resources/shaders/MaterialABI.hlsli";
+	};
+
+	class UProgramBuilder {
+	public:
+		static bool BuildFromBody(
+			const ProgramBuildInput& input,
+			GeneratedProgram&        out,
+			std::string*             errMsg = nullptr
+		);
+	};
+}
