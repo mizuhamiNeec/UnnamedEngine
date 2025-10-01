39f1fdc9350a87015b479e69933ad619e3bedb80
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-10-01 19:41:34 +0900

    ✨ ウィンドウメッセージ処理の改善と新しいプラットフォームイベントインターフェースの追加

---
+++ src/engine/public/platform/PlatformEventsImpl.cpp
@@ -0,0 +1,31 @@
+﻿#include "PlatformEventsImpl.h"
+
+#include <engine/public/IWin32MsgListener.h>
+
+namespace Unnamed {
+	bool PlatformEventsImpl::AddListener(IWin32MsgListener* listener) {
+		if (!listener) {
+			return false;
+		}
+		mListeners.push_back(listener);
+		return true;
+	}
+
+	void PlatformEventsImpl::RemoveListener(IWin32MsgListener* listener) {
+		std::erase(mListeners, listener);
+	}
+
+	bool PlatformEventsImpl::DispatchMessage(
+		const HWND   hWnd,
+		const UINT   uMsg,
+		const WPARAM wParam,
+		const LPARAM lParam
+	) {
+		for (auto* listener : mListeners) {
+			if (listener->OnWin32Message(hWnd, uMsg, wParam, lParam)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
--- src/engine/public/platform/WindowManager.h
+++ src/engine/public/platform/WindowManager.h
@@ -16,8 +16,8 @@ public:
 	Window& CreateNewWindow(const WindowDesc& desc);
 	void    DestroyWindow(uint32_t windowId);
 
-	std::unordered_map<uint32_t, Window>& GetWindows();
-	std::optional<std::reference_wrapper<Window>> Get(const uint32_t windowId);
+	std::unordered_map<uint32_t, Window>&         GetWindows();
+	std::optional<std::reference_wrapper<Window>> Get(uint32_t windowId);
 
 private:
 	HINSTANCE                            mHInstance;
