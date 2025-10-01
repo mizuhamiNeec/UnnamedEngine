0a7992cf76e044ae5ad0b15ec44fa750d89ed26e
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-10-01 20:31:14 +0900

    ✨ UEngineクラスの実装: ゲームエンジンの初期化、更新、シャットダウン機能を追加

---
+++ src/engine/public/uengine/UEngine.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include <memory>
+#include <vector>
+
+#include <engine/public/gameframework/world/UWorld.h>
+#include <engine/public/platform/PlatformEventsImpl.h>
+#include <engine/public/subsystem/interface/ISubsystem.h>
+#include <engine/public/subsystem/render/URenderSubsystem.h>
+#include <engine/public/subsystem/window/Win32/Win32WindowSystem.h>
+#include <engine/public/urenderer/GraphicsDevice.h>
+#include <engine/public/urootsignaturecache/RootSignatureCache.h>
+#include <engine/public/uuploadarena/UploadArena.h>
+
+class TimeSystem;
+
+namespace Unnamed {
+	class UInputSystem;
+
+	class UEngine {
+	public:
+		UEngine();
+		~UEngine();
+
+		void Run();
+
+	private:
+		void Init();
+		void Tick();
+		void Shutdown() const;
+
+	private:
+		std::vector<std::unique_ptr<ISubsystem>> mSubsystems;
+
+		std::unique_ptr<PlatformEventsImpl> mPlatformEvents;
+
+		std::unique_ptr<UAssetManager>         mAssetManager;
+		std::unique_ptr<GraphicsDevice>        mGraphicsDevice;
+		std::unique_ptr<RenderResourceManager> mRenderResourceManager;
+		std::unique_ptr<ShaderLibrary>         mShaderLibrary;
+		std::unique_ptr<RootSignatureCache>    mRootSignatureCache;
+		std::unique_ptr<UPipelineCache>        mPipelineCache;
+		std::unique_ptr<UploadArena>           mUploadArena;
+
+		std::unique_ptr<UWorld> mWorld;
+
+		TimeSystem*        mTime;
+		Win32WindowSystem* mWindowSystem;
+		UInputSystem*      mInputSystem;
+		URenderSubsystem*  mRenderer;
+
+		TransformComponent* mCameraTransform = nullptr;
+		UCameraComponent*   mCamera          = nullptr;
+	};
+}
