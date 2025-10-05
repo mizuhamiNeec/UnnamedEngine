2e8b84b6e4686777502c16c24917a07ec6c57982
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-10-05 20:43:35 +0900

    ✨ ShapeCast.hの修正: OverlapAtStartメソッドを追加し、ComputeImpactPointメソッドの実装を追加

--- src/engine/uphysics/ShapeCast.h
+++ src/engine/uphysics/ShapeCast.h
@@ -4,14 +4,35 @@
 namespace UPhysics {
 	struct ShapeCast {
 		virtual                             ~ShapeCast() = default;
-		[[nodiscard]] virtual Unnamed::AABB ExpandNode(const Unnamed::AABB& nodeBounds) const = 0;
+		[[nodiscard]] virtual Unnamed::AABB ExpandNode(
+			const Unnamed::AABB& nodeBounds) const = 0;
 
 		virtual bool TestTriangle(
 			const Unnamed::Triangle& tri,
-			const Vec3&     dir,
-			float           length,
-			float&          toi,
-			Vec3&           normal
+			const Vec3&              dir,
+			float                    length,
+			float&                   toi,
+			Vec3&                    normal
 		) const = 0;
+
+		// 開始時(t=0)オーバーラップ検出
+		// 重なっていれば true。depth はめり込み量（不明なら 0）
+		virtual bool OverlapAtStart(
+			const Unnamed::Triangle& tri,
+			float&                   depth,
+			Vec3&                    normal
+		) const = 0;
+
+		virtual Vec3 ComputeImpactPoint(
+			const Vec3& start,
+			const Vec3& dirNormalized,
+			float       length,
+			float       toi,
+			const Vec3& normal
+		) const {
+			(void)normal;
+			const float travel = toi * length;
+			return start + dirNormalized * travel;
+		}
 	};
 }
