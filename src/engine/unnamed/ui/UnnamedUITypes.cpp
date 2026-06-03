bc27604b0bcd8f6d955262a85731f4518401e936
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-06-03 23:49:57 +0900

    ✨: UIColor構造体を追加し、色の線形補間を行うLerp関数を実装

--- src/engine/unnamed/ui/UnnamedUITypes.cpp
+++ src/engine/unnamed/ui/UnnamedUITypes.cpp
@@ -1,1 +1,12 @@
 ﻿#include "UnnamedUITypes.h"
+
+namespace Unnamed::UI {
+	UIColor Lerp(const UIColor& a, const UIColor& b, const float t) {
+		return UIColor{
+			.r = a.r + (b.r - a.r) * t,
+			.g = a.g + (b.g - a.g) * t,
+			.b = a.b + (b.b - a.b) * t,
+			.a = a.a + (b.a - a.a) * t
+		};
+	}
+}
--- src/engine/unnamed/ui/UnnamedUITypes.h
+++ src/engine/unnamed/ui/UnnamedUITypes.h
@@ -7,6 +7,8 @@ namespace Unnamed::UI {
 		Vec2 position = Vec2::zero;
 		Vec2 size     = Vec2::one;
 
+		/// @brief 矩形内に点が含まれるか返します。
+		/// @param point クライアント座標系の点（左上が原点、右下が正方向）
 		[[nodiscard]] bool Contains(const Vec2 point) const {
 			return point.x >= position.x &&
 			       point.y >= position.y &&
@@ -14,8 +16,19 @@ namespace Unnamed::UI {
 			       point.y <= position.y + size.y;
 		}
 	};
-
+	
+	/// @brief UIの色構造体
+	/// RGBAで0.0f～1.0fの範囲で表現されます。
 	struct UIColor {
 		float r, g, b, a = 1.0f;
+	};
+
+	UIColor Lerp(const UIColor& a, const UIColor& b, float t);
+	
+	/// @brief UIテキストの水平配置です。
+	enum class UI_TEXT_ALIGN : uint8_t {
+		LEFT,
+		CENTER,
+		RIGHT,
 	};
 }
