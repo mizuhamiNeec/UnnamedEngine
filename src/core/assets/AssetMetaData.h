ab12d8a807a8c27658969cbf20edbea3b0df270e
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-07-02 16:34:44 +0900

    🔧: AssetMetaDataにロード失敗フラグを追加

--- src/core/assets/AssetMetaData.h
+++ src/core/assets/AssetMetaData.h
@@ -13,6 +13,7 @@ namespace Unnamed {
 		uint32_t    strongRefs = 0; // 外部からの参照
 		uint32_t    version    = 0; // アセットのバージョン
 		bool        loaded     = false;
+		bool        loadFailed = false; // 最後のファイルロードが失敗したか
 		bool        runtime    = false; // CreateRuntimeAssetで生成されたアセット
 		bool        destroyed  = false; // 明示破棄済みのruntimeアセット
 	};
